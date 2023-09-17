e72f72bf65479588d0bb4ee30fbd741c69d2ef6d
Marco Cruz <clm5001530@est.univalle.edu>
2023-09-16 21:28:52 -0300

    Marco Cruz Subiendo proyecto

---
+++ Menus/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Menus;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Menus/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Menus;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
